b6d5e2e3f59b6199484b46dd526fa3f2c98e42d2
BayajidAlam <bayajidalam2001@gmail.com>
2024-05-11 13:51:22 +0600

    chore: product review section done

---
+++ src/components/Product/ProductCoreDetails.tsx
@@ -0,0 +1,145 @@
+import React from "react";
+
+const CoreDetails = () => {
+  return (
+    <div>
+      <div className="space-y-4 text-[16px]">
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat.
+        </p>
+        <ol
+          style={{
+            listStyleType: "circle",
+            marginLeft: "20px",
+          }}
+        >
+          <li>Any Product types that You want - Simple, Configurable</li>
+          <li>Downloadable/Digital Products, Virtual Products</li>
+          <li>Inventory Management with Backordered items</li>
+        </ol>
+
+        <p>
+          Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+          enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi
+          ut aliquip ex ea commodo consequat. Sed do eiusmod tempor incididunt
+          ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis
+          nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+          consequat. Sed do eiusmod tempor incididunt ut labore et dolore magna
+          aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+          laboris nisi ut aliquip ex ea commodo consequat.
+        </p>
+      </div>
+
+      <div className="py-8">
+        <h1 className="pb-3 text-[20px]">Additional Info</h1>
+        <table className="w-full border rounded-xl">
+          <tr className="w-full border">
+            <td className="w-[40%] border-r py-2 px-3">
+              <p className="text-base">Stand Up</p>
+            </td>
+            <td className="w-[60%] py-2 px-3">
+              <p className="text-base">
+                35″L x 24″W x 37-45″H(front to back wheel)
+              </p>
+            </td>
+          </tr>
+          <tr className="w-full border">
+            <td className="w-[40%] border-r py-2 px-3">
+              <p className="text-base">Folded (w/o wheels)</p>
+            </td>
+            <td className="w-[60%] py-2 px-3">
+              <p className="text-base">32.5″L x 18.5″W x 16.5″H</p>
+            </td>
+          </tr>
+          <tr className="w-full border">
+            <td className="w-[40%] border-r py-2 px-3">
+              <p className="text-base">Door Pass Through</p>
+            </td>
+            <td className="w-[60%] py-2 px-3">
+              <p className="text-base">24</p>
+            </td>
+          </tr>
+          <tr className="w-full border">
+            <td className="w-[40%] border-r py-2 px-3">
+              <p className="text-base">24</p>
+            </td>
+            <td className="w-[60%] py-2 px-3">
+              <p className="text-base">Aluminum</p>
+            </td>
+          </tr>
+          <tr className="w-full border">
+            <td className="w-[40%] border-r py-2 px-3">
+              <p className="text-base">Weight (w/o wheels)</p>
+            </td>
+            <td className="w-[60%] py-2 px-3">
+              <p className="text-base">20 LBS</p>
+            </td>
+          </tr>
+          <tr className="w-full border">
+            <td className="w-[40%] border-r py-2 px-3">
+              <p className="text-base">Weight Capacity</p>
+            </td>
+            <td className="w-[60%] py-2 px-3">
+              <p className="text-base">Weight Capacity</p>
+            </td>
+          </tr>
+          <tr className="w-full border">
+            <td className="w-[40%] border-r py-2 px-3">
+              <p className="text-base">Width</p>
+            </td>
+            <td className="w-[60%] py-2 px-3">
+              <p className="text-base">24″</p>
+            </td>
+          </tr>
+          <tr className="w-full border">
+            <td className="w-[40%] border-r py-2 px-3">
+              <p className="text-base">Handle height (ground to handle)</p>
+            </td>
+            <td className="w-[60%] py-2 px-3">
+              <p className="text-base">37-45″</p>
+            </td>
+          </tr>
+          <tr className="w-full border">
+            <td className="w-[40%] border-r py-2 px-3">
+              <p className="text-base">Wheels</p>
+            </td>
+            <td className="w-[60%] py-2 px-3">
+              <p className="text-base">21.5″</p>
+            </td>
+          </tr>
+          <tr className="w-full border">
+            <td className="w-[40%] border-r py-2 px-3">
+              <p className="text-base">Head room (inside canopy)</p>
+            </td>
+            <td className="w-[60%] py-2 px-3">
+              <p className="text-base">25″</p>
+            </td>
+          </tr>
+          <tr className="w-full border">
+            <td className="w-[40%] border-r py-2 px-3">
+              <p className="text-base">Color</p>
+            </td>
+            <td className="w-[60%] py-2 px-3">
+              <p className="text-base">Black, Blue, Red, White</p>
+            </td>
+          </tr>
+          <tr className="w-full border">
+            <td className="w-[40%] border-r py-2 px-3">
+              <p className="text-base">Size</p>
+            </td>
+            <td className="w-[60%] py-2 px-3">
+              <p className="text-base">M, S</p>
+            </td>
+          </tr>
+        </table>
+      </div>
+    </div>
+  );
+};
+
+export default CoreDetails;
---
+++ src/components/Product/ProductReviews.tsx
@@ -0,0 +1,369 @@
+import React from "react";
+import { FaStar } from "react-icons/fa";
+import UserImage from "@/assets/demo1_en.png";
+import Image from "next/image";
+import { AiOutlineDislike, AiOutlineLike } from "react-icons/ai";
+const ProductReviews = () => {
+  return (
+    <div className="space-y-4">
+      <div className="space-y-2">
+        <div className="flex justify-between items-start ">
+          <div className="flex justify-start items-start gap-1">
+            <Image
+              className="rounded-[50%] h-10 w-10"
+              src={UserImage}
+              width={50}
+              height={50}
+              alt="user image"
+            />
+            <div>
+              <h1>Bayajid Alam Joyel</h1>
+              <div className="flex justify-start items-center gap-1">
+                <FaStar className="text-orange-400" />
+                <FaStar className="text-orange-400" />
+                <FaStar className="text-orange-400" />
+                <FaStar className="text-orange-400" />
+                <FaStar className="text-orange-400" />
+              </div>
+            </div>
+          </div>
+          <p>2 weeks ago</p>
+        </div>
+
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae cumque
+          quisquam nesciunt rem amet laudantium, mollitia aut dolorum,
+          consequuntur, officia culpa facere? Magnam alias quos ad nostrum
+          repudiandae repellendus est inventore dolore qui dignissimos
+          reiciendis illum, enim quidem ex corporis eum maxime quaerat
+          architecto ipsum blanditiis temporibus distinctio et provident!
+        </p>
+
+        <div className="flex justify-start items-center gap-1">
+          <Image
+            className="rounded-lg"
+            src={UserImage}
+            width={100}
+            height={100}
+            alt="user image"
+          />
+          <Image
+            className="rounded-lg"
+            src={UserImage}
+            width={100}
+            height={100}
+            alt="user image"
+          />
+          <Image
+            className="rounded-lg"
+            src={UserImage}
+            width={100}
+            height={100}
+            alt="user image"
+          />
+          <Image
+            className="rounded-lg"
+            src={UserImage}
+            width={100}
+            height={100}
+            alt="user image"
+          />
+          <Image
+            className="rounded-lg"
+            src={UserImage}
+            width={100}
+            height={100}
+            alt="user image"
+          />
+        </div>
+        <div className="flex justify-between items-center gap-2">
+          <p>
+            Was this review helpfutl? <button>Yes</button> <button>No</button>
+          </p>
+          <div className="flex justify-between items-center gap-1">
+            <div className="flex justify-between items-center gap-1">
+              <AiOutlineLike className="text-xl hover:text-[#08c]" />
+              (10)
+            </div>
+
+            <div className="flex justify-between items-center gap-1">
+              <AiOutlineDislike className="text-xl hover:text-[#08c]" />
+              (0)
+            </div>
+          </div>
+        </div>
+
+        <hr />
+      </div>
+      <div className="space-y-2">
+        <div className="flex justify-between items-start ">
+          <div className="flex justify-start items-start gap-1">
+            <Image
+              className="rounded-[50%] h-10 w-10"
+              src={UserImage}
+              width={50}
+              height={50}
+              alt="user image"
+            />
+            <div>
+              <h1>Bayajid Alam Joyel</h1>
+              <div className="flex justify-start items-center gap-1">
+                <FaStar className="text-orange-400" />
+                <FaStar className="text-orange-400" />
+                <FaStar className="text-orange-400" />
+                <FaStar className="text-orange-400" />
+                <FaStar className="text-orange-400" />
+              </div>
+            </div>
+          </div>
+          <p>2 weeks ago</p>
+        </div>
+
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae cumque
+          quisquam nesciunt rem amet laudantium, mollitia aut dolorum,
+          consequuntur, officia culpa facere? Magnam alias quos ad nostrum
+          repudiandae repellendus est inventore dolore qui dignissimos
+          reiciendis illum, enim quidem ex corporis eum maxime quaerat
+          architecto ipsum blanditiis temporibus distinctio et provident!
+        </p>
+
+        <div className="flex justify-start items-center gap-1">
+          <Image
+            className="rounded-lg"
+            src={UserImage}
+            width={100}
+            height={100}
+            alt="user image"
+          />
+          <Image
+            className="rounded-lg"
+            src={UserImage}
+            width={100}
+            height={100}
+            alt="user image"
+          />
+          <Image
+            className="rounded-lg"
+            src={UserImage}
+            width={100}
+            height={100}
+            alt="user image"
+          />
+          <Image
+            className="rounded-lg"
+            src={UserImage}
+            width={100}
+            height={100}
+            alt="user image"
+          />
+          <Image
+            className="rounded-lg"
+            src={UserImage}
+            width={100}
+            height={100}
+            alt="user image"
+          />
+        </div>
+        <div className="flex justify-between items-center gap-2">
+          <p>
+            Was this review helpfutl? <button>Yes</button> <button>No</button>
+          </p>
+          <div className="flex justify-between items-center gap-1">
+            <div className="flex justify-between items-center gap-1">
+              <AiOutlineLike className="text-xl hover:text-[#08c]" />
+              (10)
+            </div>
+
+            <div className="flex justify-between items-center gap-1">
+              <AiOutlineDislike className="text-xl hover:text-[#08c]" />
+              (0)
+            </div>
+          </div>
+        </div>
+
+        <hr />
+      </div>
+      <div className="space-y-2">
+        <div className="flex justify-between items-start ">
+          <div className="flex justify-start items-start gap-1">
+            <Image
+              className="rounded-[50%] h-10 w-10"
+              src={UserImage}
+              width={50}
+              height={50}
+              alt="user image"
+            />
+            <div>
+              <h1>Bayajid Alam Joyel</h1>
+              <div className="flex justify-start items-center gap-1">
+                <FaStar className="text-orange-400" />
+                <FaStar className="text-orange-400" />
+                <FaStar className="text-orange-400" />
+                <FaStar className="text-orange-400" />
+                <FaStar className="text-orange-400" />
+              </div>
+            </div>
+          </div>
+          <p>2 weeks ago</p>
+        </div>
+
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae cumque
+          quisquam nesciunt rem amet laudantium, mollitia aut dolorum,
+          consequuntur, officia culpa facere? Magnam alias quos ad nostrum
+          repudiandae repellendus est inventore dolore qui dignissimos
+          reiciendis illum, enim quidem ex corporis eum maxime quaerat
+          architecto ipsum blanditiis temporibus distinctio et provident!
+        </p>
+
+        <div className="flex justify-start items-center gap-1">
+          <Image
+            className="rounded-lg"
+            src={UserImage}
+            width={100}
+            height={100}
+            alt="user image"
+          />
+          <Image
+            className="rounded-lg"
+            src={UserImage}
+            width={100}
+            height={100}
+            alt="user image"
+          />
+          <Image
+            className="rounded-lg"
+            src={UserImage}
+            width={100}
+            height={100}
+            alt="user image"
+          />
+          <Image
+            className="rounded-lg"
+            src={UserImage}
+            width={100}
+            height={100}
+            alt="user image"
+          />
+          <Image
+            className="rounded-lg"
+            src={UserImage}
+            width={100}
+            height={100}
+            alt="user image"
+          />
+        </div>
+        <div className="flex justify-between items-center gap-2">
+          <p>
+            Was this review helpfutl? <button>Yes</button> <button>No</button>
+          </p>
+          <div className="flex justify-between items-center gap-1">
+            <div className="flex justify-between items-center gap-1">
+              <AiOutlineLike className="text-xl hover:text-[#08c]" />
+              (10)
+            </div>
+
+            <div className="flex justify-between items-center gap-1">
+              <AiOutlineDislike className="text-xl hover:text-[#08c]" />
+              (0)
+            </div>
+          </div>
+        </div>
+
+        <hr />
+      </div>
+      <div className="space-y-2">
+        <div className="flex justify-between items-start ">
+          <div className="flex justify-start items-start gap-1">
+            <Image
+              className="rounded-[50%] h-10 w-10"
+              src={UserImage}
+              width={50}
+              height={50}
+              alt="user image"
+            />
+            <div>
+              <h1>Bayajid Alam Joyel</h1>
+              <div className="flex justify-start items-center gap-1">
+                <FaStar className="text-orange-400" />
+                <FaStar className="text-orange-400" />
+                <FaStar className="text-orange-400" />
+                <FaStar className="text-orange-400" />
+                <FaStar className="text-orange-400" />
+              </div>
+            </div>
+          </div>
+          <p>2 weeks ago</p>
+        </div>
+
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae cumque
+          quisquam nesciunt rem amet laudantium, mollitia aut dolorum,
+          consequuntur, officia culpa facere? Magnam alias quos ad nostrum
+          repudiandae repellendus est inventore dolore qui dignissimos
+          reiciendis illum, enim quidem ex corporis eum maxime quaerat
+          architecto ipsum blanditiis temporibus distinctio et provident!
+        </p>
+
+        <div className="flex justify-start items-center gap-1">
+          <Image
+            className="rounded-lg"
+            src={UserImage}
+            width={100}
+            height={100}
+            alt="user image"
+          />
+          <Image
+            className="rounded-lg"
+            src={UserImage}
+            width={100}
+            height={100}
+            alt="user image"
+          />
+          <Image
+            className="rounded-lg"
+            src={UserImage}
+            width={100}
+            height={100}
+            alt="user image"
+          />
+          <Image
+            className="rounded-lg"
+            src={UserImage}
+            width={100}
+            height={100}
+            alt="user image"
+          />
+          <Image
+            className="rounded-lg"
+            src={UserImage}
+            width={100}
+            height={100}
+            alt="user image"
+          />
+        </div>
+        <div className="flex justify-between items-center gap-2">
+          <p>
+            Was this review helpfutl? <button>Yes</button> <button>No</button>
+          </p>
+          <div className="flex justify-between items-center gap-1">
+            <div className="flex justify-between items-center gap-1">
+              <AiOutlineLike className="text-xl hover:text-[#08c]" />
+              (10)
+            </div>
+
+            <div className="flex justify-between items-center gap-1">
+              <AiOutlineDislike className="text-xl hover:text-[#08c]" />
+              (0)
+            </div>
+          </div>
+        </div>
+
+        <hr />
+      </div>
+    </div>
+  );
+};
+
+export default ProductReviews;
